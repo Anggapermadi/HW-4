9f57b65a799df48694f4ff080b87073315a64e17
Anggapermadi <permadiangga040101@gmail.com>
2023-01-28 23:39:25 +0700

    tugas HW4

---
+++ index.js
@@ -0,0 +1,11 @@
+let angka = Math.floor(Math.random() * 50) + 1;
+console.log(angka);
+
+Math.floor(Math.random() * 10);
+
+let randomArray = [];
+for (let i = 0; i < 100; i++) {
+  let angka = Math.floor(Math.random() * 50) + 1;
+  randomArray.push(angka);
+}
+console.log(randomArray);
